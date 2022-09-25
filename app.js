7a181020de1af7aa526888a6adab3bb3fc232632
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-09-25 14:47:24 +0600

    initial server setup

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+/* Set Middle wares  */
+app.use(cors());
+app.use(express.json());
+
+/* Testing api  */
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
+/* Not Found Routes */
+app.use((req, res, next) => {
+  res.status(404).send({ success: false, message: "No Route Found " });
+});
+
+/* Server Error Routes */
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .send({ success: false, message: "Something Broken of your API" });
+});
+
+module.exports = app;
