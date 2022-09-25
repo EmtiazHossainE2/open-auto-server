7e6a7c6ee0e3040a410bbb3569bf1bf5113113c9
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-09-25 16:19:27 +0600

    backend handle

--- Controllers/v1/userRecord.controller.js
+++ Controllers/v1/userRecord.controller.js
@@ -2,37 +2,14 @@ const client = require("../../utils/connection.js");
 const recordsCollection = client.db("openAuto").collection("usersRecord");
 
 exports.getUserRecord = async(req,res) => {
-  try {
-    const allRecord = await recordsCollection.find().toArray();
-    res.status(200).json({
-      success: true,
-      allRecord
-    })
-  } catch (error) {
-    res.status(400).json({
-      message: "Something is wrong",
-      status: 400,
-      error: error.message,
-    });
-  }
+  const allRecord = await recordsCollection.find({}).toArray();
+  res.send(allRecord)
 }
 
 // Create New Record 
 exports.newRecord = async(req,res) => {
-  try {
-    const result = await recordsCollection.insertOne(req.body)
-    res.status(201).json({
-      message: "Record created successfully",
-      status: 201,
-      result
-    });
-
-  } catch (error) {
-    res.status(400).json({
-      message: "Record not created",
-      status: 400,
-      error: error.message,
-    });
-  }
+  const data = req.body;
+  const result = await recordsCollection.insertOne(data);
+  res.send(result);
 }
 
